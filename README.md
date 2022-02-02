f0efa1df4193535dfa2ed0809e5956b0a1e5a046
Phillip <philsher93@gmail.com>
2022-02-02 18:04:42 -0500

    Lots of updates- added CLI options and a new bot

---
+++ README.md
@@ -0,0 +1,13 @@
+How to use:
+
+Currently you might have to change source code to get the right mouse positions.
+
+How to run: npm start
+
+Supported Command Line args:
+
+- npm start alch (runs alch bot)
+
+- npm start positions (gets mouse position for you to use in source code)
+
+- npm start steal (steals from a stall)
--- index.js
+++ index.js
@@ -1,24 +1,70 @@
+/* 
+
+TODO
+
+add TS support
+make inputs passable from command line or let the use click where they want instead of finding own positions
+
+*/
+
 var robot = require("robotjs");
 
 // todo use args
 
-// const myArgs = process.argv.slice(2);
-// console.log('myArgs: ', myArgs);
+const args = process.argv.slice(2);
 
+switch (args[0]) {
+  case "positions":
+    getPosition();
+    break;
+  case "alch":
+    loop(alch, 3);
+    break;
+  case "steal":
+    steal();
+}
 // TODO add as param
 // set moveMouse to where the mouse should go
-function alch_clicks() {
+function alch() {
   console.log("time:", new Date());
   robot.moveMouse(1615, 882);
   robot.mouseClick("left", true);
 }
 
-(function loop() {
-  var rand = 3000.0 + (Math.floor(Math.random() * 6) + 1) * Math.random();
+function steal() {
+  const positionStall = [873, 555];
+
+  const positionItem = [1478, 801];
+
+  loop(() => {
+    robot.moveMouse(...positionStall);
+    robot.mouseClick("left", false);
+  }, 3);
+
+  loop(() => {
+    robot.keyToggle("shift", "down");
+    robot.moveMouse(...positionItem);
+    robot.mouseClick("left");
+    robot.keyToggle("shift", "up");
+  }, 4.5);
+}
+
+// move the mouse to where I want to know the position
+function getPosition() {
+  while (true) {
+    console.log(robot.getMousePos());
+  }
+}
+
+// loops on a semi-random interval (fn: function to run, sec: seconds)
+function loop(fn, sec) {
+  var rand = sec * 1000.0 + (Math.floor(Math.random() * 6) + 1) * Math.random();
+  fn();
   setTimeout(function () {
-    alch_clicks();
-    loop();
+    console.log(fn);
+    fn();
+    loop(fn, sec);
   }, rand);
-})();
+}
 
 // 1615 882
