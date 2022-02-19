02abf5ca765e4b4fa670eec0c0922aea49891c6d
Phillip <philsher93@gmail.com>
2022-02-19 01:58:06 -0500

    improvements

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 
 TODO
 
-add TS support
-make inputs passable from command line or let the use click where they want instead of finding own positions
+- add TS support
+- make inputs passable from command line or let the use click where they want instead of finding own positions
+- setup only works on RL, my monitor, with my postitions. you will need to update the hard coded values - 
+- make the magic numbers consts
 
 */
 
 var robot = require("robotjs");
+
+const ALCH_TIMEOUT = 3;
+const TELE_ALCH_TIMEOUT = 3.2;
+const POSITION_STALL = [589, 605];
+const POSITION_ITEMS = [1478, 801];
+const ALCH_POSITION = [1615, 882];
+const STOP_POSITION = [1159, 470];
+const STOP_PIXEL_COLOR = "03e9a9";
 
 // todo use args
 
@@ -15,13 +25,13 @@ const args = process.argv.slice(2);
 
 switch (args[0]) {
   case "positions":
-    getPosition();
+    scan();
     break;
   case "alch":
-    loop(alch, 3);
+    loop(alch, ALCH_TIMEOUT);
     break;
   case "tele-alch":
-    loop(alchTeleCamelot, 3);
+    loop(alchTeleCamelot, TELE_ALCH_TIMEOUT);
     break;
   case "steal":
     steal();
@@ -32,7 +42,7 @@ switch (args[0]) {
 // set moveMouse to where the mouse should go
 function alch() {
   console.log("time:", new Date());
-  robot.moveMouse(1615, 882);
+  robot.moveMouse(...ALCH_POSITION);
   robot.mouseClick("left", true);
 }
 
@@ -47,12 +57,8 @@ function alchTeleCamelot() {
 }
 
 function steal() {
-  const positionStall = [589, 605];
-
-  const positionItem = [1478, 801];
-
   loop(() => {
-    robot.moveMouse(...positionStall);
+    robot.moveMouse(...POSITION_STALL);
     console.log("moved mouse");
     robot.mouseClick("left", false);
     console.log("left clicked");
@@ -61,7 +67,7 @@ function steal() {
   loop(() => {
     robot.keyToggle("shift", "down");
     console.log("toggled shift down");
-    robot.moveMouse(...positionItem);
+    robot.moveMouse(...POSITION_ITEMS);
     console.log("moved mouse");
     robot.mouseClick("left", false);
     console.log("left clicked");
@@ -70,10 +76,14 @@ function steal() {
   }, 4.5);
 }
 
-// move the mouse to where I want to know the position
-function getPosition() {
+// move the mouse to where I want to know the position/color
+function scan() {
   while (true) {
+    checkForStopCondition();
+    const position = robot.getMousePos();
+    const { x, y } = position;
     console.log(robot.getMousePos());
+    console.log(robot.getPixelColor(x, y));
   }
 }
 
@@ -82,8 +92,21 @@ function loop(fn, sec) {
   var rand = sec * 1000.0 + (Math.floor(Math.random() * 6) + 1) * Math.random();
   setTimeout(function () {
     fn();
+    checkForStopCondition();
     loop(fn, sec);
   }, rand);
 }
 
-// 1615 882
+function checkForStopCondition() {
+  console.log(STOP_PIXEL_COLOR);
+  console.log(STOP_POSITION);
+  const stopPixelColor = robot.getPixelColor(
+    STOP_POSITION[0],
+    STOP_POSITION[1]
+  );
+  console.log("MUSIC BUTTON COLOR:", stopPixelColor);
+  // halt condition
+  if (stopPixelColor === STOP_PIXEL_COLOR) {
+    process.exit();
+  }
+}
